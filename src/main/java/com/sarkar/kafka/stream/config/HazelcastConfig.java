655137b859b1b497f1d318176d77eac7fa508729
ssarkar74 <ssarkar74@hotmail.com>
2025-02-08 15:47:27 -0500

    Caching

--- src/main/java/com/sarkar/kafka/stream/config/HazelcastConfig.java
+++ src/main/java/com/sarkar/kafka/stream/config/HazelcastConfig.java
@@ -1,7 +1,6 @@
 package com.sarkar.kafka.stream.config;
 
 import com.hazelcast.config.*;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
